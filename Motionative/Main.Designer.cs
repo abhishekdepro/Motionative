dadda81ef680af33f9889b95d6e70c5ec3fef056
Abhishek De <abhishekde@hotmail.com>
2013-07-24 08:17:04 +0530

    Code

---
+++ Motionative/Main.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Motionative
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diagramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conversionChartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lawsOfMotionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectiliniearMotionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gravityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectileMotionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newtonsLawsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.curvilinearMotionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectileAndGravityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.momentumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frictionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTxtSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.sEARCHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LblDateTime = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.lawsOfMotionToolStripMenuItem,
+            this.projectileAndGravityToolStripMenuItem,
+            this.aboutUsToolStripMenuItem,
+            this.toolStripTxtSearch,
+            this.sEARCHToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(755, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graphToolStripMenuItem,
+            this.diagramToolStripMenuItem,
+            this.calculatorToolStripMenuItem,
+            this.conversionChartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(43, 23);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // graphToolStripMenuItem
+            // 
+            this.graphToolStripMenuItem.Name = "graphToolStripMenuItem";
+            this.graphToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.graphToolStripMenuItem.Text = "Graph";
+            // 
+            // diagramToolStripMenuItem
+            // 
+            this.diagramToolStripMenuItem.Name = "diagramToolStripMenuItem";
+            this.diagramToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.diagramToolStripMenuItem.Text = "Diagram";
+            // 
+            // calculatorToolStripMenuItem
+            // 
+            this.calculatorToolStripMenuItem.Name = "calculatorToolStripMenuItem";
+            this.calculatorToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.calculatorToolStripMenuItem.Text = "Calculator";
+            this.calculatorToolStripMenuItem.Click += new System.EventHandler(this.calculatorToolStripMenuItem_Click);
+            // 
+            // conversionChartToolStripMenuItem
+            // 
+            this.conversionChartToolStripMenuItem.Name = "conversionChartToolStripMenuItem";
+            this.conversionChartToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.conversionChartToolStripMenuItem.Text = "Conversion Chart";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lawsOfMotionToolStripMenuItem
+            // 
+            this.lawsOfMotionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectiliniearMotionToolStripMenuItem,
+            this.gravityToolStripMenuItem,
+            this.projectileMotionToolStripMenuItem,
+            this.newtonsLawsToolStripMenuItem,
+            this.curvilinearMotionToolStripMenuItem});
+            this.lawsOfMotionToolStripMenuItem.Name = "lawsOfMotionToolStripMenuItem";
+            this.lawsOfMotionToolStripMenuItem.Size = new System.Drawing.Size(101, 23);
+            this.lawsOfMotionToolStripMenuItem.Text = "Laws of Motion";
+            // 
+            // rectiliniearMotionToolStripMenuItem
+            // 
+            this.rectiliniearMotionToolStripMenuItem.Name = "rectiliniearMotionToolStripMenuItem";
+            this.rectiliniearMotionToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.rectiliniearMotionToolStripMenuItem.Text = "Rectiliniear Motion";
+            // 
+            // gravityToolStripMenuItem
+            // 
+            this.gravityToolStripMenuItem.Name = "gravityToolStripMenuItem";
+            this.gravityToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.gravityToolStripMenuItem.Text = "Gravity";
+            // 
+            // projectileMotionToolStripMenuItem
+            // 
+            this.projectileMotionToolStripMenuItem.Name = "projectileMotionToolStripMenuItem";
+            this.projectileMotionToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.projectileMotionToolStripMenuItem.Text = "Projectile Motion";
+            // 
+            // newtonsLawsToolStripMenuItem
+            // 
+            this.newtonsLawsToolStripMenuItem.Name = "newtonsLawsToolStripMenuItem";
+            this.newtonsLawsToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.newtonsLawsToolStripMenuItem.Text = "Newton\'s laws";
+            // 
+            // curvilinearMotionToolStripMenuItem
+            // 
+            this.curvilinearMotionToolStripMenuItem.Name = "curvilinearMotionToolStripMenuItem";
+            this.curvilinearMotionToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.curvilinearMotionToolStripMenuItem.Text = "Curvilinear Motion";
+            // 
+            // projectileAndGravityToolStripMenuItem
+            // 
+            this.projectileAndGravityToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.momentumToolStripMenuItem,
+            this.frictionToolStripMenuItem});
+            this.projectileAndGravityToolStripMenuItem.Name = "projectileAndGravityToolStripMenuItem";
+            this.projectileAndGravityToolStripMenuItem.Size = new System.Drawing.Size(48, 23);
+            this.projectileAndGravityToolStripMenuItem.Text = "Force";
+            // 
+            // momentumToolStripMenuItem
+            // 
+            this.momentumToolStripMenuItem.Name = "momentumToolStripMenuItem";
+            this.momentumToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.momentumToolStripMenuItem.Text = "Momentum";
+            // 
+            // frictionToolStripMenuItem
+            // 
+            this.frictionToolStripMenuItem.Name = "frictionToolStripMenuItem";
+            this.frictionToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.frictionToolStripMenuItem.Text = "Friction";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(67, 23);
+            this.aboutUsToolStripMenuItem.Text = "About us";
+            // 
+            // toolStripTxtSearch
+            // 
+            this.toolStripTxtSearch.Name = "toolStripTxtSearch";
+            this.toolStripTxtSearch.Size = new System.Drawing.Size(100, 23);
+            // 
+            // sEARCHToolStripMenuItem
+            // 
+            this.sEARCHToolStripMenuItem.Name = "sEARCHToolStripMenuItem";
+            this.sEARCHToolStripMenuItem.Size = new System.Drawing.Size(63, 23);
+            this.sEARCHToolStripMenuItem.Text = "SEARCH";
+            // 
+            // LblDateTime
+            // 
+            this.LblDateTime.AutoSize = true;
+            this.LblDateTime.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblDateTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblDateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDateTime.Location = new System.Drawing.Point(619, 0);
+            this.LblDateTime.Name = "LblDateTime";
+            this.LblDateTime.Size = new System.Drawing.Size(50, 26);
+            this.LblDateTime.TabIndex = 2;
+            this.LblDateTime.Text = "Date";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 445);
+            this.Controls.Add(this.LblDateTime);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Motionative 1.0.0";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diagramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conversionChartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lawsOfMotionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectiliniearMotionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gravityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectileMotionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newtonsLawsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem curvilinearMotionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectileAndGravityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem momentumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frictionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTxtSearch;
+        private System.Windows.Forms.ToolStripMenuItem sEARCHToolStripMenuItem;
+        private System.Windows.Forms.Label LblDateTime;
+    }
+}
+
---
+++ Motionative/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using Calculator;
+
+namespace Motionative
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            LblDateTime.Text = System.DateTime.Now.Date.ToString("dd-MMM-yyyy");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calculatorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+              
+        }
+    }
+}
